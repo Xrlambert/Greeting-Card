6d2684b4b89ff14ec476f2da438a8104b668e9e0
Xrlambert <xavierlambertt@gmail.com>
2025-12-20 23:18:47 -0500

    Add project files.

---
+++ Greeting Card/Form1.cs
@@ -0,0 +1,14 @@
+namespace Greeting_Card
+{
+    public partial class Form1 : Form
+    {
+
+        bool showCover = true;
+        Random rand = new Random();
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+    }
+}
